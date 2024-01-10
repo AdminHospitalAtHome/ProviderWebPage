96141ba17450551a950c22ef1d7e47be142fa701
Yunzhe Wei <weiy5@rose-hulman.edu>
2024-01-10 17:51:05 -0500

    get patient by provider ID
    Co-authored-by: rhit-ken<yangfan544@gmail.com>

--- ProviderWebPage/BackendFunctionCall/Patient/getPatientList.ts
+++ ProviderWebPage/BackendFunctionCall/Patient/getPatientList.ts
@@ -8,3 +8,14 @@ export function getAllPatients(): Promise<any> {
         });
     });
   }
+
+export function getAllPatientsByProviderID(providerID: number): Promise<any> {
+    const url = `https://hosptial-at-home-js-api.azurewebsites.net/api/getPatientsByProviderID?providerID=${providerID}`;
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(response => response.json())
+            .then(json => {
+                resolve(json);
+            });
+    });
+}
