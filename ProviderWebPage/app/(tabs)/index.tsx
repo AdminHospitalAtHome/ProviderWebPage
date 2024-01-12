a9a9ba21e7169364af6c1fcf9550e9cf64ea328e
rhit-wallisjm <wallisjm@rose-hulman.edu>
2024-01-12 15:50:19 -0500

    Display Chat Working
    Co-Authored-By: rhit-jiangy10 <90019374+jiangy10@users.noreply.github.com>

--- ProviderWebPage/app/(tabs)/index.tsx
+++ ProviderWebPage/app/(tabs)/index.tsx
@@ -1,163 +1,107 @@
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
-import { ChatClient } from "@azure/communication-chat";
-import { AzureCommunicationTokenCredential } from '@azure/communication-common';
+import {Text, View} from '../../components/Themed';
+import {ChatClient, ChatThreadClient} from "@azure/communication-chat";
+import {AzureCommunicationTokenCredential} from '@azure/communication-common';
+
 const endpointurl =
   'https://hospitalathomechat.unitedstates.communication.azure.com';
 
 
 import {
+  getAllMessages,
   getChatThread,
   getCommunicationId,
   getCommunicationToken,
-  getMessage,
-  sendMessage,
+  initChatClient, initChatThreadClient,
+  sendMessage, temp_communicationId,
 } from '../BackendFunctions/Chat/Message'
 import React, {useCallback, useEffect, useState} from 'react';
 import {Bubble, GiftedChat} from 'react-native-gifted-chat';
 
 
 export default function TabOneScreen() {
-  const [accessToken, setAccessToken] = useState('');
-  const [communicationId, setCommunicationId] = useState('');
-  const [chatThreadId, setChatThreadId] = useState('');
-  const [chatMessage, setChatMessage] = useState<any[]>([]);
-  const [giftedChatMessages, setGiftedChatMessages] = useState<any[]>([]);
+  // Temporary until we get the provider one set up
+  const providerUserID = 200000001
   const [chatClient, setChatClient] = useState<ChatClient | undefined>(undefined);
-  const [messageCounter, setMessageCounter] = useState<number>(1);
+  const [chatThreadClient, setChatThreadClient] = useState<ChatThreadClient | undefined>(undefined)
+  const [chatMessages, setChatMessages] = useState<any[]>([]) // TODO: Change to specific type later....
 
   useEffect(() => {
-    getCommunicationId(200000001)
-      .then(res => {
-        setCommunicationId(res);
-      })
-      .catch(error => {
-        console.error(error);
-      });
+    console.log("RUN")
+    initChatClient(providerUserID).then(res => {
+      setChatClient(res)
+    });
   }, []);
 
   useEffect(() => {
-    if (communicationId) {
-      getCommunicationToken(communicationId)
-        .then(res => {
-          setAccessToken(res);
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    }
-  }, [communicationId]);
-
-  useEffect(() => {
-    if (accessToken) {
-      setChatClient(new ChatClient(endpointurl, new AzureCommunicationTokenCredential(accessToken)));
-      if (chatClient) {
-
-        chatClient.startRealtimeNotifications();
-        chatClient.on('chatMessageReceived', (e) => {
-          setMessageCounter(messageCounter + 1);
-
-        })
-      }
-    }
-  }, [accessToken]);
-
-  useEffect(() => {
-    if (accessToken && communicationId) {
-      getChatThread(communicationId)
-        .then(res => {
-          setChatThreadId(res);
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    }
-  }, [accessToken, communicationId]);
-
-  useEffect(() => {
-    if (chatThreadId && accessToken) {
-      getMessage(chatThreadId, accessToken).then(res => {
-        let parsedMsg = [];
-
-        for (let i = 0; i < res.value.length; i++) {
-          if (!res.value[i].content.message) {
-            continue;
-          }
-          let dic = {
-            _id: res.value[i].id,
-            text: res.value[i].content.message,
-            createdAt: res.value[i].createdOn,
-            user: {
-              _id: res.value[i].senderCommunicationIdentifier.communicationUser
-                .id,
-              name: 'Patient',
-              avatar:
-                'https://pbs.twimg.com/profile_images/1276003977910517761/SOFf-4mY_400x400.jpg',
-            },
-          };
-          parsedMsg.push(dic);
+    if (chatClient) {
+      initChatThreadClient(chatClient).then(res => {
+        setChatThreadClient(res)
+        if (res) {
+          console.log('created chat thread client')
+        } else {
+          console.log('issue')
         }
-        setChatMessage(res.value);
-        setGiftedChatMessages(parsedMsg);
-        console.log(messageCounter)
       });
     }
-  }, [chatThreadId, accessToken, messageCounter]);
+  }, [chatClient]);
 
-  const onSend = useCallback(
-    (messages:any[] = []) => {
-      if (chatThreadId && accessToken) {
-        sendMessage(chatThreadId, accessToken, messages[0].text);
-        setGiftedChatMessages((previousMessages: any) =>
-          GiftedChat.append(previousMessages, messages),
-        );
-      }
-    },
-    [chatThreadId, accessToken],
-  );
-
-  const renderBubble = (props: React.JSX.Element) => {
+  useEffect(() => {
+    if (chatThreadClient) {
+      getAllMessages(chatThreadClient).then((res) => {
+        setChatMessages(res);
+        console.log(res);
+        console.log(temp_communicationId)
+      })
+    }
+  }, [chatThreadClient])
+  if (chatThreadClient) {
     return (
-      <Bubble
-        {...props}
-        wrapperStyle={{
-          left: {
-            backgroundColor: '#e6dfdf',
-          },
-        }}
-      />
-    );
-  };
+        <GiftedChat
+          messages={chatMessages}
+          onSend={() => {
+          }}
+          user={
+            {_id: temp_communicationId}
+          }
+          showAvatarForEveryMessage={true}
+        />
+    )
+  } else {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    )
+  }
 
-  return (
-    <GiftedChat
-      messages={giftedChatMessages}
-      onSend={giftedChatMessages => onSend(giftedChatMessages)}
-      user={{
-        _id: communicationId,
-      }}
-      textInputStyle={{color: 'black'}}
-      renderBubble={renderBubble}
-      showAvatarForEveryMessage={true}
-    />
-  );
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+const styles
+  =
+  StyleSheet.create
+  ({
+    container:
+      {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }
+    ,
+    title:
+      {
+        fontSize: 20,
+        fontWeight: 'bold',
+      }
+    ,
+    separator:
+      {
+        marginVertical: 30,
+        height: 1,
+        width: '80%',
+      }
+    ,
+  });
--- ProviderWebPage/app/BackendFunctions/Chat/Message.ts
+++ ProviderWebPage/app/BackendFunctions/Chat/Message.ts
@@ -1,7 +1,46 @@
-import { ChatClient } from "@azure/communication-chat";
+import {ChatClient, ChatMessage, ChatThreadClient} from "@azure/communication-chat";
+import {AzureCommunicationTokenCredential} from "@azure/communication-common";
 
 const endpointurl =
   'https://hospitalathomechat.unitedstates.communication.azure.com';
+export let temp_communicationId: string = ''
+
+
+
+export function initChatClient(userId: number): Promise<ChatClient | undefined> {
+  return new Promise<ChatClient | undefined>((resolve) => {
+    getCommunicationId(userId).then(res => {
+      temp_communicationId = res
+      console.log(temp_communicationId);
+      getCommunicationToken(res).then(accessToken => {
+        resolve(new ChatClient(endpointurl, new AzureCommunicationTokenCredential(accessToken)));
+      }).catch(() => {
+        console.error("Failed to init chat client");
+        resolve(undefined);
+      });
+    }).catch(() => {
+      console.error("Failed to init chat client");
+      resolve(undefined);
+    });
+  });
+}
+
+export function initChatThreadClient(chatClient: ChatClient): Promise<ChatThreadClient | undefined> {
+  return new Promise(resolve => {
+    console.log("AAAA" + temp_communicationId)
+    if (temp_communicationId !== '') {
+      getChatThread(temp_communicationId).then(res => {
+        resolve(chatClient.getChatThreadClient(res))
+      }).catch(() => {
+        resolve(undefined);
+      })
+    } else {
+      console.error("no communication id set")
+      resolve(undefined);
+    }
+  })
+
+}
 
 export function getCommunicationId(userId: number): Promise<string> {
   return new Promise((resolve, reject) => {
@@ -20,10 +59,10 @@ export function getCommunicationId(userId: number): Promise<string> {
 }
 
 //should modify later when the provider site sis ready, call it on contact page
-export function getChatThread(userId: string): Promise<string> {
+export function getChatThread(communicationId: string): Promise<string> {
   return new Promise((resolve, reject) => {
     fetch(
-      `https://hosptial-at-home-js-api.azurewebsites.net/api/getChatThread?UserId=${userId}`,
+      `https://hosptial-at-home-js-api.azurewebsites.net/api/getChatThread?UserId=${communicationId}`,
     )
       .then(response => response.json())
       .then(res => {
@@ -32,10 +71,10 @@ export function getChatThread(userId: string): Promise<string> {
   });
 }
 
-export function getCommunicationToken(userId: string): Promise<string> {
+export function getCommunicationToken(communicationId: string): Promise<string> {
   return new Promise((resolve, reject) => {
     fetch(
-      `https://hosptial-at-home-js-api.azurewebsites.net/api/getUserToken?userId=${userId}`,
+      `https://hosptial-at-home-js-api.azurewebsites.net/api/getUserToken?userId=${communicationId}`,
     )
       .then(res => res.json())
       .then(res => {
@@ -44,19 +83,56 @@ export function getCommunicationToken(userId: string): Promise<string> {
   });
 }
 
-export function getMessage(
-  threadId: string,
-  accessToken: string,
-): Promise<any> {
-  return new Promise((resolve, reject) => {
-    fetch(
-      `https://hospitalathomechat.unitedstates.communication.azure.com/chat/threads/${threadId}/messages?api-version=2023-11-07`,
-      {method: 'GET', headers: {Authorization: `Bearer ${accessToken}`}},
-    )
-      .then(res => res.json())
-      .then(res => resolve(res));
+//todo: rewrite
+
+// export function getMessage(
+//   threadId: string,
+//   accessToken: string,
+// ): Promise<any> {
+//   return new Promise((resolve, reject) => {
+//     fetch(
+//       `https://hospitalathomechat.unitedstates.communication.azure.com/chat/threads/${threadId}/messages?api-version=2023-11-07`,
+//       {method: 'GET', headers: {Authorization: `Bearer ${accessToken}`}},
+//     )
+//       .then(res => res.json())
+//       .then(res => resolve(res));
+//   });
+// }
+
+
+export function getAllMessages(chatThreadClient: ChatThreadClient): Promise<any[]> {
+  return new Promise(async resolve => {
+
+    const messages = chatThreadClient.listMessages()
+
+    let parsedMessages = [];
+
+    let communicatorID
+
+    resolve(parsedMessages);
+
+
+    for await (const m of messages) {
+      // Incase it is a different kind of message like user added to chat or topic changed...
+      try {
+        let dictionary = {
+          _id: m.id,
+          text: m.content.message,
+          createdAt: m.createdOn,
+          user: {
+            _id: m.sender.communicationUserId,
+            name: 'TEMPROARY',
+          }
+        }
+        parsedMessages.push(dictionary);
+      } catch {
+
+      }
+    }
   });
 }
+
+
 
 export function sendMessage(
   threadId: string,
